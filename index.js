da5265ebec60afa4145abbdd3730556a32f135b0
Jesse Smart <jsmartdev@protonmail.com>
2022-08-01 16:41:32 -0400

    parsed data, setup package.json, installed chart.js

---
+++ index.js
@@ -0,0 +1,13 @@
+const getData = async () => {
+  const response = await fetch("data/data.csv");
+  const data = await response.text();
+  const table = data.split(/\n/).slice(1);
+  table.forEach((row) => {
+    const columns = row.split(",");
+    const year = columns[0];
+    const temp = columns[1];
+    console.log(year, temp);
+  });
+};
+
+getData();
