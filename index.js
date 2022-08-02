28aaf48d7663323adfe52103a001a93704dd370e
Jesse Smart <jsmartdev@protonmail.com>
2022-08-02 06:19:19 -0400

    fetch, parse, and graph data

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+const xlabels = [];
+const ytemps = [];
+
 const getData = async () => {
   const response = await fetch("data/data.csv");
   const data = await response.text();
@@ -5,9 +8,39 @@ const getData = async () => {
   table.forEach((row) => {
     const columns = row.split(",");
     const year = columns[0];
+    xlabels.push(year);
     const temp = columns[1];
+    ytemps.push(parseFloat(temp) + 14);
     console.log(year, temp);
   });
 };
 
-getData();
+const chartIt = async () => {
+  await getData();
+  const ctx = document.getElementById("myChart").getContext("2d");
+  Chart.defaults.font.size = 14;
+  const myChart = new Chart(ctx, {
+    type: "line",
+    data: {
+      labels: xlabels,
+      datasets: [
+        {
+          label: "Global Average Temperature",
+          data: ytemps,
+          backgroundColor: "lightgrey",
+          borderColor: "black",
+          borderWidth: 1,
+        },
+      ],
+    },
+    options: {
+      scales: {
+        y: {
+          beginAtZero: false,
+        },
+      },
+    },
+  });
+};
+
+chartIt();
